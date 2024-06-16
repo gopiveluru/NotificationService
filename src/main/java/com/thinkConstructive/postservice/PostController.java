168cd1485e0b9681354a83ab7bc104176ae70c17
gopiveluru <gopiveluru@gmail.com>
2024-06-16 17:00:04 +0530

    first commit1

--- src/main/java/com/thinkConstructive/postservice/PostController.java
+++ src/main/java/com/thinkConstructive/postservice/PostController.java
@@ -5,9 +5,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 @RequestMapping("/posts")
 public class PostController {
+
+    @GetMapping("/")
+    public String welcomeToUser(Principal principal)
+    {
+        return "Welcome "+principal.getName()+"to Post Service";
+    }
+
     @GetMapping(value = "{postId}")
     public Posts getPosts(@PathVariable("postId") String postId)
     {
